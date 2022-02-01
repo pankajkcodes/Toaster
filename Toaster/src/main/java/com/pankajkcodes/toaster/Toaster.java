728783eeac43f505b3fe076f1d64faaa0da767aa
pankajkcodes <pankajkcodes@gmail.com>
2022-02-01 18:36:41 +0530

    Initial commit

---
+++ Toaster/src/main/java/com/pankajkcodes/toaster/Toaster.java
@@ -0,0 +1,11 @@
+package com.pankajkcodes.toaster;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+    public static void simpleToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ app/src/main/java/com/pankajkcodes/toaster/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pankajkcodes.toaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toaster.simpleToast(this,"This is simple toast of toaster library");
+    }
+}
